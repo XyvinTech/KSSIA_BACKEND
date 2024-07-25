a774edf0356f06c11c08ef66be6ac2b240b787a8
Brighty Jiji Abraham <brightymct@gmail.com>
2024-07-26 03:17:14 +0530

    Update adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -40,23 +40,34 @@ exports.createUser = async (req, res) => {
 exports.editUser = async (req, res) => {
     try {
         const { userId } = req.params;
+        const { membership_id } = req.params;
         const data = req.body;
 
         // Validate the input data
-        const { error } = EditUserSchema.validate(data, {
-            abortEarly: true
-        });
+        const { error } = EditUserSchema.validate(data, { abortEarly: true });
 
         // Check if an error exists in the validation
         if (error) {
             return responseHandler(res, 400, `Invalid input: ${error.message}`);
         }
 
-        // Find and update the user
-        const updatedUser = await User.findByIdAndUpdate(userId, data, { new: true, runValidators: true });
+        if(userId){
+            // Find and update the user
+            const updatedUser = await User.findByIdAndUpdate(userId, data, { new: true, runValidators: true });
 
-        if (!updatedUser) {
-            return responseHandler(res, 404, "User not found");
+            if (!updatedUser) {
+                return responseHandler(res, 404, "User not found");
+            }
+        }
+        else if(membership_id){
+             // Find and update the user
+             const updatedUser = await User.findOneAndUpdate(membership_id, data, { new: true, runValidators: true });
+
+             if (!updatedUser) {
+                 return responseHandler(res, 404, "User not found");
+             }
+        }else{
+            return responseHandler(res, 400, `Invalid input: ${error.message}`);
         }
 
         return responseHandler(res, 200, "User updated successfully!", updatedUser);
@@ -84,21 +95,24 @@ exports.deleteUser = async (req, res) => {
     try {
         const { userId } = req.params;
         const { membership_id } = req.params;
+
+        // If the parameter requested is userId or membership_id
         if(userId){
+            // Find and delete the user
             const user = await User.findByIdAndDelete(userId);
             if (!user) {
                 return responseHandler(res, 404, "User not found");
             }
-        }
-        else if(membership_id){
+        } else if(membership_id){
+            // Find and delete the user
             const user = await User.findOneAndDelete(membership_id);
             if (!user) {
                 return responseHandler(res, 404, "User not found");
             }
-        }
-        else{
+        } else{
             return responseHandler(res, 400, `Invalid input: ${error.message}`);
         }
+        
         return responseHandler(res, 200, "User deleted successfully");
     } catch (error) {
         console.error(error);
