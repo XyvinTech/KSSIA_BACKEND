c9689d8a9ef050ba32657cecdfd455e116b1f6c1
Brighty Jiji Abraham <brightymct@gmail.com>
2024-08-18 16:40:24 +0530

    Update promotionController.js

--- src/controllers/promotionController.js
+++ src/controllers/promotionController.js
@@ -45,9 +45,9 @@ exports.createPromotion = async (req, res) => {
 
     // Handle file upload if present
     const bucketName = process.env.AWS_S3_BUCKET;
-    let banner_image_url = promotion.banner_image_url;
-    let upload_video = promotion.upload_video;
-    let poster_image_url = promotion.poster_image_url;
+    let banner_image_url = '';
+    let upload_video = '';
+    let poster_image_url = '';
 
     if (req.file) {
         if (data.type === 'banner') {
