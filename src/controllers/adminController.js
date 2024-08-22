d0644b01dc2dc3efc8c3ddf467c5f8fed318c433
hello-world-ttj <tijotjoseph003@gmail.com>
2024-08-22 12:54:43 +0530

    #restructure

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -11,39 +11,41 @@ const { CreateUserSchema, EditUserSchema } = require("../validation");
 /****************************************************************************************************/
 
 exports.createUser = async (req, res) => {
-  
-    const data = req.body;
-    // console.log(`Received data parameter: ${data}`);                                 // Debug line
+  const data = req.body;
+  // console.log(`Received data parameter: ${data}`);                                 // Debug line
 
-    // Validate the input data
-    const { error } = CreateUserSchema.validate(data, {
-        abortEarly: true
-    });
+  // Validate the input data
+  const { error } = CreateUserSchema.validate(data, {
+    abortEarly: true,
+  });
 
-    // Check if an error exists in the validation
-    if (error) {
-        // If an error exists, return a 400 status code with the error message
-        // console.log('Invalid input: ${error.message}');                              // Debug line
-        return responseHandler(res, 400, `Invalid input: ${error.message}`);
-    }
+  // Check if an error exists in the validation
+  if (error) {
+    // If an error exists, return a 400 status code with the error message
+    // console.log('Invalid input: ${error.message}');                              // Debug line
+    return responseHandler(res, 400, `Invalid input: ${error.message}`);
+  }
 
-    // Check if a user with this mobile number already exists
-    const existingUser = await User.findOne({
-        'phone_numbers.personal': data.phone_numbers.personal
-    });
-    if (existingUser) {
-        // If a user with this mobile number already exists, return a 400 status code with the error message
-        // console.log('User with this phone number already exists');                   // Debug line
-        return responseHandler(res, 409, `User with this phone number already exists`);
-    }
+  // Check if a user with this mobile number already exists
+  const existingUser = await User.findOne({
+    "phone_numbers.personal": data.phone_numbers.personal,
+  });
+  if (existingUser) {
+    // If a user with this mobile number already exists, return a 400 status code with the error message
+    // console.log('User with this phone number already exists');                   // Debug line
+    return responseHandler(
+      res,
+      409,
+      `User with this phone number already exists`
+    );
+  }
 
-    // Create a new user
-    const newUser = new User(data);
-    await newUser.save();
+  // Create a new user
+  const newUser = new User(data);
+  await newUser.save();
 
-    // console.log(`New user created: ${newUser}`);                                   // Debug line
-    return responseHandler(res, 201, `New user created successfully!`, newUser);
-   
+  // console.log(`New user created: ${newUser}`);                                   // Debug line
+  return responseHandler(res, 201, `New user created successfully!`, newUser);
 };
 
 /****************************************************************************************************/
@@ -51,51 +53,55 @@ exports.createUser = async (req, res) => {
 /****************************************************************************************************/
 
 exports.editUser = async (req, res) => {
-    
-    const { userId } = req.params;
-    const { membership_id } = req.params;
-    const data = req.body;
-    // console.log(`Received userId parameter: ${userId}`);                             // Debug line
-    // console.log(`Received membership_id parameter: ${membership_id}`);               // Debug line
-    // console.log(`Received data parameter: ${data}`);                                 // Debug line
+  const { userId } = req.params;
+  const { membership_id } = req.params;
+  const data = req.body;
+  // console.log(`Received userId parameter: ${userId}`);                             // Debug line
+  // console.log(`Received membership_id parameter: ${membership_id}`);               // Debug line
+  // console.log(`Received data parameter: ${data}`);                                 // Debug line
 
-    // Validate the input data
-    const { error } = EditUserSchema.validate(data, { abortEarly: true });
+  // Validate the input data
+  const { error } = EditUserSchema.validate(data, { abortEarly: true });
 
-    // Check if an error exists in the validation
-    if (error) {
-        // If an error exists, return a 400 status code with the error message
-        // console.log('Invalid input: ${error.message}');                              // Debug line
-        return responseHandler(res, 400, `Invalid input: ${error.message}`);
+  // Check if an error exists in the validation
+  if (error) {
+    // If an error exists, return a 400 status code with the error message
+    // console.log('Invalid input: ${error.message}');                              // Debug line
+    return responseHandler(res, 400, `Invalid input: ${error.message}`);
+  }
+
+  if (userId) {
+    // Find and update the user using userId
+    const updatedUser = await User.findByIdAndUpdate(userId, data, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedUser) {
+      // If the user is not found, return a 404 status code with the error message
+      // console.log('User not found');                                           // Debug line
+      return responseHandler(res, 404, "User not found");
     }
+  } else if (membership_id) {
+    // Find and update the user using membership_id
+    const updatedUser = await User.findOneAndUpdate(membership_id, data, {
+      new: true,
+      runValidators: true,
+    });
 
-    if (userId) {
-        // Find and update the user using userId
-        const updatedUser = await User.findByIdAndUpdate(userId, data, { new: true, runValidators: true });
-
-        if (!updatedUser) {
-            // If the user is not found, return a 404 status code with the error message
-            // console.log('User not found');                                           // Debug line
-            return responseHandler(res, 404, "User not found");
-        }
-    } else if (membership_id) {
-        // Find and update the user using membership_id
-        const updatedUser = await User.findOneAndUpdate(membership_id, data, { new: true, runValidators: true });
-
-        if (!updatedUser) {
-            // If the user is not found, return a 404 status code with the error message
-            // console.log('User not found');                                           // Debug line
-            return responseHandler(res, 404, "User not found");
-        }
-    } else {
-        // If neither userId nor membership_id is provided, return a 400 status code with the error
-        // console.log('Invalid request');                                              // Debug line
-        return responseHandler(res, 400, `Invalid request`);
+    if (!updatedUser) {
+      // If the user is not found, return a 404 status code with the error message
+      // console.log('User not found');                                           // Debug line
+      return responseHandler(res, 404, "User not found");
     }
+  } else {
+    // If neither userId nor membership_id is provided, return a 400 status code with the error
+    // console.log('Invalid request');                                              // Debug line
+    return responseHandler(res, 400, `Invalid request`);
+  }
 
-    // console.log('User updated successfully!');                                       // Debug line
-    return responseHandler(res, 200, "User updated successfully!", updatedUser);
-
+  // console.log('User updated successfully!');                                       // Debug line
+  return responseHandler(res, 200, "User updated successfully!", updatedUser);
 };
 
 /****************************************************************************************************/
@@ -103,90 +109,86 @@ exports.editUser = async (req, res) => {
 /****************************************************************************************************/
 
 const deleteUserFiles = async (user) => {
-    // Delete user's files
-    if (user.profile_picture) {
-        let oldFileKey = path.basename(user.profile_picture);
-        await deleteFile(bucketName, oldFileKey);
-    }
+  // Delete user's files
+  if (user.profile_picture) {
+    let oldFileKey = path.basename(user.profile_picture);
+    await deleteFile(bucketName, oldFileKey);
+  }
 
-    if (user.company_logo){
-        let oldFileKey = path.basename(user.company_logo);
-        await deleteFile(bucketName, oldFileKey);
-    }
+  if (user.company_logo) {
+    let oldFileKey = path.basename(user.company_logo);
+    await deleteFile(bucketName, oldFileKey);
+  }
 
-    if (user.certificates) {
-        for (const cert of user.certificates) {
-            let oldFileKey = path.basename(cert.url);
-            await deleteFile(bucketName, oldFileKey);
-        }
+  if (user.certificates) {
+    for (const cert of user.certificates) {
+      let oldFileKey = path.basename(cert.url);
+      await deleteFile(bucketName, oldFileKey);
     }
+  }
 
-    if (user.brochure) {
-        for (const brochure of user.brochure) {
-            let oldFileKey = path.basename(brochure.url);
-            await deleteFile(bucketName, oldFileKey);
-        }
+  if (user.brochure) {
+    for (const brochure of user.brochure) {
+      let oldFileKey = path.basename(brochure.url);
+      await deleteFile(bucketName, oldFileKey);
     }
+  }
 
-    if (user.awards) {
-        for (const award of user.awards) {
-            let oldFileKey = path.basename(award.url);
-            await deleteFile(bucketName, oldFileKey);
-        }
+  if (user.awards) {
+    for (const award of user.awards) {
+      let oldFileKey = path.basename(award.url);
+      await deleteFile(bucketName, oldFileKey);
     }
+  }
 };
 
 exports.deleteUser = async (req, res) => {
+  const { userId } = req.params;
+  const { membership_id } = req.params;
+  // console.log(`Received userId: ${userId}`);                                       // Debug line
+  // console.log(`Received membership_id: ${membership_id}`);                         // Debug line
+  let user;
 
-    const { userId } = req.params;
-    const { membership_id } = req.params;
-    // console.log(`Received userId: ${userId}`);                                       // Debug line
-    // console.log(`Received membership_id: ${membership_id}`);                         // Debug line
-    let user;
-
-    if (userId) {
-        // Find and delete the user using userId
-        const user = await User.findByIdAndDelete(userId);
-        if (!user) {
-            // If the user is not found, return a 404 status code with the error message
-            // console.log('User not found');                                           // Debug line
-            return responseHandler(res, 404, "User not found");
-        }
-
-        await deleteUserFiles(user);
-
-    } else if (membership_id) {
-        // Find and delete the user using membership_id
-        const user = await User.findOneAndDelete( {membership_id: membership_id} );
-        if (!user) {
-            // If the user is not found, return a 404 status code with the error message
-            // console.log('User not found');                                           // Debug line
-            return responseHandler(res, 404, "User not found");
-        }
-
-        await deleteUserFiles(user);
-
-    } else {
-        // If neither userId nor membership_id is provided, return a 400 status code with the error
-        // console.log('Invalid request');                                              // Debug line
-        return responseHandler(res, 400, `Invalid request`);
+  if (userId) {
+    // Find and delete the user using userId
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      // If the user is not found, return a 404 status code with the error message
+      // console.log('User not found');                                           // Debug line
+      return responseHandler(res, 404, "User not found");
     }
 
-    // Delete product images
-    const products = await Product.find({ seller_id: userId });
-    for (const product of products) {
-        if (product.image_url) {
-            let oldFileKey = path.basename(product.image_url);
-            await deleteFile(bucketName, oldFileKey);
-        }
+    await deleteUserFiles(user);
+  } else if (membership_id) {
+    // Find and delete the user using membership_id
+    const user = await User.findOneAndDelete({ membership_id: membership_id });
+    if (!user) {
+      // If the user is not found, return a 404 status code with the error message
+      // console.log('User not found');                                           // Debug line
+      return responseHandler(res, 404, "User not found");
     }
 
-    // Delete products
-    await Product.deleteMany({ seller_id: userId });
+    await deleteUserFiles(user);
+  } else {
+    // If neither userId nor membership_id is provided, return a 400 status code with the error
+    // console.log('Invalid request');                                              // Debug line
+    return responseHandler(res, 400, `Invalid request`);
+  }
 
-    // console.log(`User deleted successfully`);                                        // Debug line
-    return responseHandler(res, 200, "User deleted successfully");
+  // Delete product images
+  const products = await Product.find({ seller_id: userId });
+  for (const product of products) {
+    if (product.image_url) {
+      let oldFileKey = path.basename(product.image_url);
+      await deleteFile(bucketName, oldFileKey);
+    }
+  }
 
+  // Delete products
+  await Product.deleteMany({ seller_id: userId });
+
+  // console.log(`User deleted successfully`);                                        // Debug line
+  return responseHandler(res, 200, "User deleted successfully");
 };
 
 /****************************************************************************************************/
@@ -194,11 +196,16 @@ exports.deleteUser = async (req, res) => {
 /****************************************************************************************************/
 
 exports.getAllUsers = async (req, res) => {
-  
-    const users = await User.find();
-    // console.log(users);                                                              // Debug line
-    return responseHandler(res, 200, "Users retrieved successfully", users);
-   
+  const users = await User.find();
+  const mappedData = users.map((user) => {
+    return {
+      ...user._doc,
+      name: `${user.name.first_name} ${user.name.middle_name} ${user.name.last_name}`,
+      mobile: user.phone_numbers.personal,
+    };
+  });
+  // console.log(users);                                                              // Debug line
+  return responseHandler(res, 200, "Users retrieved successfully", mappedData);
 };
 
 /****************************************************************************************************/
@@ -206,25 +213,23 @@ exports.getAllUsers = async (req, res) => {
 /****************************************************************************************************/
 
 exports.getUserById = async (req, res) => {
-    
-    const { userId } = req.params;
-    // console.log(`Received userId: ${userId}`);                                       // Debug line
-        
-    if (!userId) {
-        // If userId is not provided, return a 400 status code with the error message
-        // console.log('Invalid request');                                              // Debug line
-        return responseHandler(res, 400, `Invalid request`);
-    }
+  const { userId } = req.params;
+  // console.log(`Received userId: ${userId}`);                                       // Debug line
 
-    // Check if a user with this id exists
-    const user = await User.findById(userId);
-    if (!user) {
-        // If the user is not found, return a 404 status code with the error message
-        // console.log('User not found');                                               // Debug line
-        return responseHandler(res, 404, "User not found");
-    }
+  if (!userId) {
+    // If userId is not provided, return a 400 status code with the error message
+    // console.log('Invalid request');                                              // Debug line
+    return responseHandler(res, 400, `Invalid request`);
+  }
 
-    // console.log(`User retrieved successfully`);                                      // Debug line
-    return responseHandler(res, 200, "User retrieved successfully", user);
-   
+  // Check if a user with this id exists
+  const user = await User.findById(userId);
+  if (!user) {
+    // If the user is not found, return a 404 status code with the error message
+    // console.log('User not found');                                               // Debug line
+    return responseHandler(res, 404, "User not found");
+  }
+
+  // console.log(`User retrieved successfully`);                                      // Debug line
+  return responseHandler(res, 200, "User retrieved successfully", user);
 };
