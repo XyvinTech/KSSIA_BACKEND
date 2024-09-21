0609ca3efec198d222dd68eceebf2d9bb4e796cb
Brighty Jiji Abraham <brightymct@gmail.com>
2024-09-21 09:49:25 +0530

    Update messages.js

--- src/models/messages.js
+++ src/models/messages.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const Requirements = require('./requirements');
 
 const MessageSchema = new mongoose.Schema({
     from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
