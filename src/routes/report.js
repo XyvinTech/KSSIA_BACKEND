34431e6a912c59ae593518f597ba27523822cd0a
Brighty Jiji Abraham <brightymct@gmail.com>
2024-09-23 00:59:13 +0530

    Update report.js

--- src/routes/report.js
+++ src/routes/report.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const reportController = require("../controllers/reportController");
 const authVerify = require("../middlewares/authVerify");
+const asyncHandler = require("../utils/asyncHandler");
 const reportRoute = express.Router();
 
 reportRoute.use(authVerify);
 
 reportRoute
   .route("/")
-  .post(reportController.createReport)
-  .get(reportController.getReports);
+  .post(asyncHandler(reportController.createReport))
+  .get(asyncHandler(reportController.getReports));
 
 module.exports = reportRoute;
