25ae7303a9aa3e0c762f29ae508b17d3c6db0f51
hello-world-ttj <tijotjoseph003@gmail.com>
2024-09-30 12:30:41 +0530

    #server

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -6,7 +6,6 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
