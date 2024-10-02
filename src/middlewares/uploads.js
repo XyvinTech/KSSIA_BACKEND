ae5852c170553416b488af03f44fa6881edb02f9
hello-world-ttj <tijotjoseph003@gmail.com>
2024-10-02 08:49:43 +0530

    #limit for multer

--- src/middlewares/uploads.js
+++ src/middlewares/uploads.js
@@ -1,20 +1,24 @@
-const multer = require('multer');
+const multer = require("multer");
 
 const storage = multer.memoryStorage(); // Use memory storage to process files in memory
 
 const fileFilter = (req, file, cb) => {
-    const allowedMimeTypes = ['image/', 'video/', 'application/pdf'];
+  const allowedMimeTypes = ["image/", "video/", "application/pdf"];
 
-    if (allowedMimeTypes.some(type => file.mimetype.startsWith(type))) {
-        cb(null, true);
-    } else {
-        cb(new Error('Not an image or video! Please upload an appropriate file.'), false);
-    }
+  if (allowedMimeTypes.some((type) => file.mimetype.startsWith(type))) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error("Not an image or video! Please upload an appropriate file."),
+      false
+    );
+  }
 };
 
 const upload = multer({
-    storage,
-    fileFilter,
+  storage,
+  fileFilter,
+  limits: { fileSize: 100 * 1024 * 1024 },
 });
 
 module.exports = upload;
