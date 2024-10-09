012d6a81bdaa50a9a66892254531795db9be5c99
Brighty Jiji Abraham <brightymct@gmail.com>
2024-10-09 17:46:06 +0530

    event notifications

--- src/jobs/updateEventStatus.js
+++ src/jobs/updateEventStatus.js
@@ -2,6 +2,7 @@ const cron = require("node-cron");
 const moment = require("moment-timezone");
 const Event = require("../models/events");
 require("dotenv").config();
+const { getMessaging } = require("firebase-admin/messaging");
 
 // Define the cron job to run every minute
 cron.schedule("* * * * *", async () => {
@@ -19,6 +20,41 @@ cron.schedule("* * * * *", async () => {
       { status: "live" },
       { new: true }
     );
+
+    try {
+      liveEvents.forEach( async event => {
+        const topic = `event_${event._id}`;
+        const message = {
+          notification: {
+            title: `Event ${event.name} is now live!`,
+            body: `The event ${event.name} has started. Join now!`,
+          },
+          topic: topic,
+          android: {
+            notification: {
+              imageUrl: event.image,
+            },
+          },
+          apns: {
+            payload: {
+              aps: {
+                "mutable-content": 1,
+              },
+            },
+            fcm_options: {
+              imageUrl: event.image,
+            },
+          },
+        };
+        
+        // Send the notification using the Firebase Cloud Messaging (FCM) service
+        await getMessaging().send(message);
+
+      });  
+    } catch (error) {
+      console.log(`error creating notification : ${error}`);
+    }
+
     console.log(`Updated ${liveEvents.modifiedCount} events to live`);
 
     // Update events from "live" to "completed"
@@ -31,6 +67,41 @@ cron.schedule("* * * * *", async () => {
       { status: "completed" },
       { new: true }
     );
+
+    try {
+      completedEvents.forEach(async event => {
+        const topic = `event_${event._id}`;
+        const message = {
+          notification: {
+            title: `Event ${event.name} is now completed!`,
+            body: `The event ${event.name} has ended. Thank you for participating!`,
+          },
+          topic: topic,
+          android: {
+            notification: {
+              imageUrl: event.image,
+            },
+          },
+          apns: {
+            payload: {
+              aps: {
+                "mutable-content": 1,
+              },
+            },
+            fcm_options: {
+              imageUrl: event.image,
+            },
+          },
+        };
+        
+        // Send the notification using the Firebase Cloud Messaging (FCM) service
+        await getMessaging().send(message);
+      
+      });  
+    } catch (error) {
+      console.log(`error creating notification : ${error}`);
+    }
+
     console.log(`Updated ${completedEvents.modifiedCount} events to completed`);
   } catch (err) {
     console.error("Error updating events:", err);
