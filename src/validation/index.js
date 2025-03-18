88cf8c5417f79c42a3ea057de6235ca0c83b4559
hello-world-ttj <tijotjoseph003@gmail.com>
2025-03-18 15:57:04 +0530

    #member_id

--- src/validation/index.js
+++ src/validation/index.js
@@ -65,6 +65,7 @@ exports.CreateUserSchema = Joi.object({
 exports.EditUserSchema = Joi.object({
   abbreviation: Joi.string(),
   name: Joi.string(),
+  membership_id: Joi.string(),
   blood_group: Joi.string().allow(""),
   email: Joi.string().email().required(),
   profile_picture: Joi.string().uri().allow(""),
